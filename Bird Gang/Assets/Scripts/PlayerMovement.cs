b9513d516ecee2281901e6810f94cefed06191ab
EmilyLB <56558688+EmilyLB@users.noreply.github.com>
2022-01-27 12:17:54 +0000

    Added wasd movement and jump for player

---
+++ Bird Gang/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody player;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        player.velocity = new Vector3(horizontalInput * 2, player.velocity.y, verticalInput * 2);
+        // Check if space key is pressed down
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 5f;
+            player.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+}
