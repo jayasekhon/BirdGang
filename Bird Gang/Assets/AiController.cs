b3b0496e4ee6b4865cda1d16740454193e9e9938
jayasekhon <60359284+jayasekhon@users.noreply.github.com>
2022-02-09 15:40:08 +0000

    fix tempremental fleeing

--- Bird Gang/Assets/AiController.cs
+++ Bird Gang/Assets/AiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class AiController : MonoBehaviour
 {
     GameObject[] goalLocations;
@@ -15,7 +16,7 @@ public class AiController : MonoBehaviour
     void ResetAgent()
     {
         speedMult = Random.Range(0.1f, 1.5f);
-        agent.speed = 2 * speedMult;
+        agent.speed = 2;
         agent.angularSpeed = 120;
         agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
     }
@@ -23,17 +24,35 @@ public class AiController : MonoBehaviour
     public void DetectNewObstacle(Vector3 position){
         if(Vector3.Distance(position, this.transform.position) < detectionRadius)
         {
+
+            
             Vector3 fleeDirection = (this.transform.position - position).normalized;
             Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
 
+
             NavMeshPath path = new NavMeshPath();
             agent.CalculatePath(newgoal, path);
+
 
             if(path.status != NavMeshPathStatus.PathInvalid)
             {
                 agent.SetDestination(path.corners[path.corners.Length - 1]);
                 agent.speed = 10;
                 agent.angularSpeed = 500;
+            } else{
+
+                NavMeshHit hit;
+                NavMeshPath newPath = new NavMeshPath();
+                float newRadius = Mathf.Infinity;
+
+                if(NavMesh.SamplePosition(newgoal, out hit, newRadius, NavMesh.AllAreas)){
+                    agent.CalculatePath(hit.position, newPath);
+                    agent.SetDestination(newPath.corners[newPath.corners.Length - 1]);
+                    agent.speed = 10;
+                    agent.angularSpeed = 500;
+
+                }
+   
             }
         }
 
@@ -52,11 +71,14 @@ public class AiController : MonoBehaviour
         agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
         agent.speed *= Random.Range(0.2f, 1.5f);
     }
+
+
     private void Update()
     {
         if (agent.remainingDistance < 2)
         {
             ResetAgent();
+
         }
     }
 }
--- Bird Gang/Assets/SpawnManager.cs
+++ Bird Gang/Assets/SpawnManager.cs
@@ -53,5 +53,6 @@ public class SpawnManager : MonoBehaviour
 
     }
 
+
     }
 }
