f2bf58b59ae59ae6a833780868287f9e76d2af10
jayasekhon <60359284+jayasekhon@users.noreply.github.com>
2022-02-10 16:07:50 +0000

    Merged targetting and AIFleeing

--- Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerController.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
+using System.IO;
 
 public class PlayerController : MonoBehaviour
 {
@@ -27,6 +28,7 @@ public class PlayerController : MonoBehaviour
 
     public GameObject targetObj;
     public GameObject Birdpoo; 
+    GameObject[] agents;
     
     void Awake()
     {
@@ -52,7 +54,7 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        Look();
+        //Look();
         Turning();
         // Move();
         // Jump();
@@ -60,17 +62,25 @@ public class PlayerController : MonoBehaviour
           * For now, take angle from camera. Also take pos from camera to be less confusing.
           * Obviously this needs to change.
           */
-         RaycastHit hit;
-         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
-         {
-             targetObj.transform.position = hit.point + Vector3.up * 0.01f;
-             if (Input.GetKeyDown("x"))
-             {
-                GameObject Birdpoo1=Instantiate(Birdpoo);
-                Birdpoo1.transform.position = rb.transform.position + Vector3.down * 0.5f;
-                hit.collider.gameObject.GetComponent<BaseBirdTarget>().OnHitByPoo();
-             }
-         }
+
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+        {
+            targetObj.transform.position = hitInfo.point + Vector3.up * 0.01f;
+
+            if(Input.GetMouseButtonDown(0)){
+                agents = GameObject.FindGameObjectsWithTag("bird_target");
+                Vector3 splatterPoint = hitInfo.point + Vector3.up * 0.01f;
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "BirdPoo"), splatterPoint, Quaternion.identity);
+                foreach(GameObject a in agents)
+                {
+                    a.GetComponent<AiController>().DetectNewObstacle(hitInfo.point);
+                }
+                hitInfo.collider.gameObject.GetComponent<BaseBirdTarget>().OnHitByPoo();
+            }
+            
+        }
     }
 
     void FixedUpdate()
