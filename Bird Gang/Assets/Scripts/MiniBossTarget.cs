0e17bea30d38df5d14685e0057f1082504a302e9
Joe Dight <joe.dight@ntmail.uk>
2022-02-14 12:33:30 +0000

    Add targeting scripts to miniboss

---
+++ Bird Gang/Assets/Scripts/MiniBossTarget.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBossTarget : BaseBirdTarget
+{
+	public override void OnHit()
+	{
+		// Do something exciting.
+	}
+}
