c7e7b61b6caf20e9a20b25ab371b68112e86b25c
EmilyLB <56558688+EmilyLB@users.noreply.github.com>
2022-03-01 10:12:45 +0000

    added custom usernames that can be seen in game

---
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerNameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField usernameInput;
+
+    public void OnUsernameInputValueChanged()
+    {
+        PhotonNetwork.NickName = usernameInput.text;
+    }
+}
