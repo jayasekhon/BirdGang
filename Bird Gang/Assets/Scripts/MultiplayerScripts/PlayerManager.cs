601d4688bcbe60336a60d708fa596d54a6136dce
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-03-22 18:00:28 +0000

    simple player camera with cinemachine

--- Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerManager.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerManager.cs
@@ -23,7 +23,7 @@ public class PlayerManager : MonoBehaviour
 
     void CreateController()
     {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(-180, 115, 115), Quaternion.Euler(0, 150, 0));
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CameraHolder"), new Vector3(0,10,-2),  Quaternion.Euler(26, 150, 0));
+        // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(-180, 115, 115), Quaternion.Euler(0, 150, 0));
+        // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CameraHolder"), new Vector3(0,10,-2),  Quaternion.Euler(26, 150, 0));
     }
 }
