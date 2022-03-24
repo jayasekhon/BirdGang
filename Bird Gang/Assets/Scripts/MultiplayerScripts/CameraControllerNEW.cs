25e2220ea9fa6da2c5e9b5e1e96dbcec6901b4cb
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-03-24 18:02:55 +0000

    small changes

--- Bird Gang/Assets/Scripts/MultiplayerScripts/CameraControllerNEW.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/CameraControllerNEW.cs
@@ -10,6 +10,11 @@ public class CameraControllerNEW : MonoBehaviour
 
     private GameObject[] playersInGame;
     private Transform playerPos;
+    CinemachineTransposer camTransposer;
+    
+    public float width = 5f;
+    public float distance = 0f;
+    public float fieldOfView = 60f;
 
     void Awake()
     {
@@ -28,13 +33,14 @@ public class CameraControllerNEW : MonoBehaviour
         }
         cam.Follow = playerPos;
         cam.LookAt = playerPos;
-    }
+        camTransposer = cam.GetCinemachineComponent<CinemachineTransposer>();
+        }
 
     void FixedUpdate()
     {
-        if (transform.position.y < 1.5f)
+        if (transform.position.y < 2.5f)
         {
-            transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 2.5f, transform.position.z);
         } 
 
     //  // When the player is moving up (so the player is facing up - positive) decrease FoV.
@@ -44,16 +50,22 @@ public class CameraControllerNEW : MonoBehaviour
             if (!(cam.m_Lens.FieldOfView <= 50))
             {
                 cam.m_Lens.FieldOfView -= 0.2f * Mathf.Abs(playerPos.forward.y);
+                // cam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = new Vector3(0, 0, -5.5f * Mathf.Tan(cam.m_Lens.FieldOfView * 0.5f * Mathf.Deg2Rad));
+
             }
         }
-        // When the player is moving down (so the player is facing down - negative) increase FoV.
+        // // When the player is moving down (so the player is facing down - negative) increase FoV.
         else if (playerPos.forward.y < -0.05f)
         {
             // Stopping the FoV getting too large
             if (!(cam.m_Lens.FieldOfView >= 75))
             {
-                cam.m_Lens.FieldOfView += 0.25f * Mathf.Abs(playerPos.forward.y);
+                cam.m_Lens.FieldOfView += 0.3f * Mathf.Abs(playerPos.forward.y);
+
+                // Mathf.Tan(cam.m_Lens.FieldOfView * 0.5f * Mathf.Deg2Rad);
+                // cam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = new Vector3(0, 0, -5.5f * Mathf.Tan(cam.m_Lens.FieldOfView * 0.5f * Mathf.Deg2Rad));
+                // cam.Body.FollowOffset = new Vector3(0, 0, -5);
             }
-        } 
+        }
     }
 }
