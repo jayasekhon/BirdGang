94878276c8840115e91a5957f266c1ebe5cb5eb7
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-03-30 21:14:35 +0100

    simply detects a flock is nearby

--- Bird Gang/Assets/Scripts/FlockHinderance.cs
+++ Bird Gang/Assets/Scripts/FlockHinderance.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FlockHinderance : MonoBehaviour
 {
     public LayerMask layerMask = ~0;
-    float radius = 2.5f; 
+    float radius = 0.5f; 
     private Rigidbody rb;
     private bool startTimer = false;
     private float originalTime = 0f;
@@ -24,29 +24,31 @@ public class FlockHinderance : MonoBehaviour
         {
             if(hitCollider.GetComponent<FlockManager>())
             {
-                // Debug.Log("hitCollider");
+                Debug.Log("hitCollider");
                 
                 // comes in when it gets hit
                 // then we start a timer for how long they can't move (kinematic = true)
                 // if the timer has already been started, don't disrupt it
                 // once the timer
-                if(startTimer == false) //so the timer hasn't been started
-                {
-                    rb.isKinematic = true;
-                    originalTime = Time.time;
-                    startTimer = true;
-                }
-                else //if the timer has been started
-                {
-                    if (originalTime - Time.time > 2f)
-                    {
-                        rb.isKinematic = false;
-                        if (originalTime - Time.time > 5f) 
-                        {
-                            startTimer = false;
-                        }
-                    }
-                }
+
+                // if(startTimer == false) //so the timer hasn't been started
+                // {
+                //     rb.isKinematic = true;
+                //     originalTime = Time.time;
+                //     startTimer = true;
+                // }
+                // else //if the timer has been started
+                // {
+                //     if (originalTime - Time.time > 2f)
+                //     {
+                //         rb.isKinematic = false;
+                //         if (originalTime - Time.time > 5f) 
+                //         {
+                //             startTimer = false;
+                //         }
+                //     }
+                // }
+
                 // rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
             }
         }
