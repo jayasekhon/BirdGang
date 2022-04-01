2bcb09e84a81682d648f783388f390a6c7352494
EmilyLB <56558688+EmilyLB@users.noreply.github.com>
2022-04-01 12:16:59 +0100

    waypoints should be working now

--- Bird Gang/Assets/Scripts/WaypointEvents.cs
+++ Bird Gang/Assets/Scripts/WaypointEvents.cs
@@ -7,13 +7,22 @@ public class WaypointEvents: MonoBehaviour
 {
     // If you have multiple custom events, it is recommended to define them in the used class
     public const byte ShowWaypoint = 6;
+    public const byte HideWaypoint = 7;
+    public const byte myPosition = 8;
     bool activeWaypoint;
     PhotonView PV;
+    int myPVID;
+    Vector3 myPos;
 
     void Awake()
     {
         PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            myPVID = PV.ViewID;
+        }
     }
+
 
     void Update()
     {
@@ -25,20 +34,35 @@ public class WaypointEvents: MonoBehaviour
         if (Input.GetMouseButtonDown(1) && !activeWaypoint)
         {
             activeWaypoint = true;
-            SendMoveUnitsToTargetPositionEvent();
-                       
+            myPos = transform.position;
+            SendMyLocation();
+            ShowMyWaypoint();                       
         }
         else if (Input.GetMouseButtonDown(1) && activeWaypoint)
         {
             activeWaypoint = false;
-            SendMoveUnitsToTargetPositionEvent();
+            myPos = transform.position;
+            SendMyLocation();
+            HideMyWaypoint();
         }
     }
 
-    private void SendMoveUnitsToTargetPositionEvent()
+    private void ShowMyWaypoint()
     {
         RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
-        PhotonNetwork.RaiseEvent(ShowWaypoint, activeWaypoint, raiseEventOptions, SendOptions.SendReliable);
+        PhotonNetwork.RaiseEvent(ShowWaypoint, myPVID, raiseEventOptions, SendOptions.SendReliable);
+    }
+    
+    private void HideMyWaypoint()
+    {
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+        PhotonNetwork.RaiseEvent(HideWaypoint, myPVID, raiseEventOptions, SendOptions.SendReliable);
+    }
+
+    private void SendMyLocation()
+    {
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+        PhotonNetwork.RaiseEvent(myPosition, myPos, raiseEventOptions, SendOptions.SendReliable);
     }
 
 
