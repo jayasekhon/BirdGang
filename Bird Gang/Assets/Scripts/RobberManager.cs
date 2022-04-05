b58539f40f3e7192f0d44e3763d891ca947b897e
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-04-05 22:18:24 +0100

    Added crowd forming in robber scene

--- Bird Gang/Assets/Scripts/RobberManager.cs
+++ Bird Gang/Assets/Scripts/RobberManager.cs
@@ -10,10 +10,12 @@ public class RobberManager : MonoBehaviour, GameEventCallbacks
 //    public static RobberManager Instance;
 
     private GameObject robber;
+    
 
     // Start is called before the first frame update
     void Awake()
     {
+        
         if (!PhotonNetwork.IsMasterClient) // checks if a RobberManager already exists
         {
             Destroy(gameObject);
@@ -27,6 +29,28 @@ public class RobberManager : MonoBehaviour, GameEventCallbacks
     public void OnStageBegin(GameEvents.Stage stage)
     {
         robber = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Robber"), new Vector3(115, 2, -280), Quaternion.identity);
+
+        //Start the navmeshagents moving to the mayors stage for the next round
+        AiController[] agents = GameObject.FindObjectsOfType<AiController>();
+        foreach(AiController agent in agents)
+        {
+            if(agent.gameObject.name!= "Robber(Clone)")
+            {
+                
+                if (Random.Range(0, 100) > 25)
+                {
+                    Vector3 minPosition = new Vector3(-20,0,-20);
+                    Vector3 maxPosition = new Vector3(20, 0, 20);
+                    Vector3 centerPosition = new Vector3(-10, 1, -233);
+                    
+                    Vector3 position = centerPosition+ new Vector3(Random.Range(minPosition.x, maxPosition.x), 0, Random.Range(minPosition.z, maxPosition.z));
+
+
+                    agent.SetGoal(position);
+                    agent.SetChangeGoal(false);
+                }
+            }
+        }
     }
 
     public void OnStageEnd(GameEvents.Stage stage)
