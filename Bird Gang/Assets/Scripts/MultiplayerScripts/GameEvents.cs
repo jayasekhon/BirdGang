ff58facfba3d6b1c37e0111474f0ded910c6ecae
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-10 14:53:16 +0100

    basic cutscenes all in

--- Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
@@ -69,8 +69,8 @@ public class GameEvents : MonoBehaviour
 	public static readonly Stage[] agenda =
 	{
 		new Stage(GAME_STAGE.TUTORIAL, 20f),
-		// new Stage(GAME_STAGE.ROBBERY, 40f),
-		// new Stage(GAME_STAGE.POLITICIAN, 40f),
+		new Stage(GAME_STAGE.ROBBERY, 40f),
+		new Stage(GAME_STAGE.POLITICIAN, 40f),
 		new Stage(GAME_STAGE.CARNIVAL, 120f),
 		new Stage(GAME_STAGE.FINISH, .01f),
 	};
