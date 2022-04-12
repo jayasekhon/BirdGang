b0cb99ea73b5c1b38f7a387ebcdab5967876934b
EmilyLB <56558688+EmilyLB@users.noreply.github.com>
2022-04-12 14:19:24 +0100

    increased time in playerindicator before searching for players

--- Bird Gang/Assets/Scripts/playerPointer.cs
+++ Bird Gang/Assets/Scripts/playerPointer.cs
@@ -33,7 +33,7 @@ public class playerPointer : MonoBehaviour
     
     IEnumerator InitCoroutine()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
 
         playersInGame = GameObject.FindGameObjectsWithTag("Player");
         InstantiateLists();
