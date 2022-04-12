d3c793ba5c41bf0860eb56f09a8797908ed4d229
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-12 18:36:21 +0100

    only master spawns brain

--- Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerManager.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/PlayerManager.cs
@@ -25,12 +25,13 @@ public class PlayerManager : MonoBehaviour
 
     void CreateController()
     {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(-180, 115, 115), Quaternion.Euler(0, 150, 0));
-        // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CameraHolder"), new Vector3(0,10,-2),  Quaternion.Euler(26, 150, 0));
-
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM Brain"), new Vector3(-185,115,125),  Quaternion.Euler(0, 150, 0));
-        // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM MainVCam"), new Vector3(-185,115,125),  Quaternion.Euler(0, 150, 0));
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM CutsceneManager"), new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
-        // object[] instantiationData = new object[] {CM_Main_cam};
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(-180, 115, 115), Quaternion.Euler(0, 150, 0)); 
+        if (PhotonNetwork.IsMasterClient) {
+            // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CameraHolder"), new Vector3(0,10,-2),  Quaternion.Euler(26, 150, 0));
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM Brain"), new Vector3(-185,115,125),  Quaternion.Euler(0, 150, 0));
+            // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM MainVCam"), new Vector3(-185,115,125),  Quaternion.Euler(0, 150, 0));
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CM CutsceneManager"), new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+            // object[] instantiationData = new object[] {CM_Main_cam};
+        }
     }
 }
