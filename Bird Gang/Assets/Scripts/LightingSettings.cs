274c730b51ad79cfca7e2761e048fb1a5364a530
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-21 00:12:06 +0100

    updated storm lighting timings

--- Bird Gang/Assets/Scripts/LightingSettings.cs
+++ Bird Gang/Assets/Scripts/LightingSettings.cs
@@ -37,8 +37,13 @@ public class LightingSettings : MonoBehaviour
     public void NightLighting()
     {
         nightTime = true;
+        StartCoroutine(ExecuteAfterTime());
+    }
+
+    IEnumerator ExecuteAfterTime()
+    {
+        yield return new WaitForSeconds(21f);        
         RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Trilight;
-        RenderSettings.ambientSkyColor = new Color(0.503293f, 0.5836419f, 0.735849f, 0f);
         RenderSettings.ambientEquatorColor = new Color(0.3897294f, 0.4619033f, 0.5471698f, 0f);
         RenderSettings.ambientGroundColor = new Color(0.2016607f, 0.1992257f, 0.3490566f, 0f);
     }
@@ -47,8 +52,9 @@ public class LightingSettings : MonoBehaviour
     {
         if (nightTime) 
         {
-            RenderSettings.skybox.SetColor("_Tint", Color.Lerp(new Color (0.5f, 0.5f, 0.5f, 1f), new Color(0.1117391f, 0.1134435f, 0.254717f,1f), step));   
-            step += Time.deltaTime/10f;
+            RenderSettings.skybox.SetColor("_Tint", Color.Lerp(new Color (0.5f, 0.5f, 0.5f, 1f), new Color(0.1117391f, 0.1134435f, 0.254717f,1f), step));
+            RenderSettings.ambientSkyColor = Color.Lerp(new Color(0.9759529f, 1f, 0.8160377f, 0f), new Color(0.503293f, 0.5836419f, 0.735849f, 0f), step);   
+            step += Time.deltaTime/12f;
         }
     }
 }
