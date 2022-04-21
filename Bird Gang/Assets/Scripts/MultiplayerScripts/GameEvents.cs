90a53aea475ceb4a4947a70accf81053e9f8187a
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-21 14:15:24 +0100

    money particles

--- Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
@@ -69,7 +69,7 @@ public class GameEvents : MonoBehaviour
 	{
 		new Stage(GAME_STAGE.INTRO, 21f),
 		new Stage(GAME_STAGE.TUTORIAL, 10f),
-		new Stage(GAME_STAGE.ROBBERY, 40f),
+		new Stage(GAME_STAGE.ROBBERY, 120f),
 		new Stage(GAME_STAGE.POLITICIAN, 40f),
 		new Stage(GAME_STAGE.CARNIVAL, 40f),
 		new Stage(GAME_STAGE.FINALE, 25f),
