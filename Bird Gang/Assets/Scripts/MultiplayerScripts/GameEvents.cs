3c3b0274752795b37557278ffbd9f6f2a4b209af
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-24 19:55:46 +0100

    correct timings again

--- Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
@@ -69,10 +69,10 @@ public class GameEvents : MonoBehaviour
 	public static readonly Stage[] serverAgenda =
 	{
 		new Stage(GAME_STAGE.INTRO, 21f),
-		new Stage(GAME_STAGE.TUTORIAL, 10f),
-		// new Stage(GAME_STAGE.ROBBERY, 80f),
-		// new Stage(GAME_STAGE.POLITICIAN, 80f),
-		new Stage(GAME_STAGE.CARNIVAL, 80f),
+		new Stage(GAME_STAGE.TUTORIAL, 120f),
+		new Stage(GAME_STAGE.ROBBERY, 120f),
+		new Stage(GAME_STAGE.POLITICIAN, 120f),
+		new Stage(GAME_STAGE.CARNIVAL, 120f),
 		new Stage(GAME_STAGE.FINALE, 25f),
 	};
 	private bool serverHasSerialised = false;
