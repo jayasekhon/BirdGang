3c03b4a8fcee8757387eb39b7f2ede0f1fa5388b
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-04-30 18:00:26 +0100

    think i fixed what broke

--- Bird Gang/Assets/Scripts/Score.cs
+++ Bird Gang/Assets/Scripts/Score.cs
@@ -170,7 +170,7 @@ public class Score : MonoBehaviour
 
     IEnumerator ExecuteAfterTime()
     {
-        yield return new WaitForSeconds(0.3f);  
+        yield return new WaitForSeconds(0.5f);  //gives it enough time to fade
         fade = false;
     }
 
