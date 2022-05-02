6400660a3c8411774bf4a0b575876aa27a8b8a03
Ibaan <ibanhossain@gmail.com>
2022-05-02 12:04:00 +0100

    audio slider + music

--- Bird Gang/Assets/Scripts/VolumeControls.cs
+++ Bird Gang/Assets/Scripts/VolumeControls.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class VolumeControls : MonoBehaviour
 {
     public GameObject SoundEffectsSliderObj;
     public GameObject MusicSliderObj;
+
+    [SerializeField] AudioMixer SFXMixer;
 
     public Slider soundEffectsVolume;
     public Slider musicVolume;
@@ -21,7 +24,10 @@ public class VolumeControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // AudioListener.volume = musicVolume.value;
+        SFXMixer.SetFloat("SFXVolume", Mathf.Log10(musicVolume.value) * 20);
+        Debug.Log(musicVolume);
+        Debug.Log(Mathf.Log10(musicVolume.value) * 20);
     }
 
     public void SoundEffectsVolumeChanged()
