2f43ab9a2a82c00236f2e5f704697496e96bc924
jayasekhon <60359284+jayasekhon@users.noreply.github.com>
2022-05-06 17:09:30 +0100

    reset times

--- Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
+++ Bird Gang/Assets/Scripts/MultiplayerScripts/GameEvents.cs
@@ -68,7 +68,7 @@ public class GameEvents : MonoBehaviour
 	public static readonly Stage[] serverAgenda =
 	{
 		new Stage(GAME_STAGE.INTRO, 21f),
-		new Stage(GAME_STAGE.TUTORIAL, 20f),
+		new Stage(GAME_STAGE.TUTORIAL, 120f),
 		new Stage(GAME_STAGE.ROBBERY, 120f),
 		new Stage(GAME_STAGE.POLITICIAN, 120f),
 		new Stage(GAME_STAGE.CARNIVAL, 120f),
