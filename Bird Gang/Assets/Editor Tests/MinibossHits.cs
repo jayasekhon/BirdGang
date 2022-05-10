dc707b4660333524192b92d469fa91d2a882272f
ambika_a <57152715+Ambikaa1@users.noreply.github.com>
2022-05-10 18:47:04 +0100

    miniboss hits tests

---
+++ Bird Gang/Assets/Editor Tests/MinibossHits.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class MinibossHits 
+{
+    [Test]
+    public void health_status_is_correct()
+    {
+        // ACT
+        string correctHealth = "+++";
+        string returnedHealth = MiniBossTarget.correctNumHealth(3);
+
+        // ASSERT
+        Assert.AreEqual(correctHealth, returnedHealth);
+    }
+
+    [Test]
+    public void health_colour_changes()
+    {
+        // ACT
+        Color32 correctColour = new Color32(119, 215, 40, 255);
+        Color32 returnedColour = MiniBossTarget.changeHealthColour();
+
+        // ASSERT
+        Assert.AreEqual(correctColour, returnedColour);
+    }
+}
